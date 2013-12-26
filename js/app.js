ef8636422d5d273e247077cdd07f76cd5a24a12e
jondthompson <jon@usestickshift.com>
2013-12-26 14:39:49 -0600

    Refactored actions to fit our existing schema.
    Does not create data and ID fields that are missing. Getting close to
    something useable.

--- js/app.js
+++ js/app.js
@@ -17,11 +17,7 @@ App.Router.map(function() {
   			this.resource("ssMenus",{path:"menus"}, function (){
   				this.resource("ssMenu",{path:":name"}, function(){
   					this.resource("ssMenuItems",{path:"menu"}, function(){
-  						this.resource("ssMenuItem", {path: ":menuID"}, function(){
-  							this.resource("ssActions", {path: "actions"}, function(){
-  								this.resource("ssAction", {path: "actionID"})
-  							})
-  						})
+  						this.resource("ssMenuItem", {path: ":menuID"})
   					})
   				})
   			})
@@ -75,36 +71,26 @@ App.SsMenuList = EmberFire.ObjectArray.extend({
   }
 });
 
-App.SsActionList = EmberFire.ObjectArray.extend({
-  firebaseURI: "https://menutest.firebaseio.com/",
-
-  init: function(){
-    var firebase = new Firebase(this.get('firebaseURI'));
-    this.set('ref', firebase);
-    this._super();
-  }
-});
-
 
 App.SSMENUITEMS = [
-        { id:"0",text: "0-0", page: 0, row: 0, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 1, text: "0-1", page: 0, row: 1, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 2, text: "0-2", page: 0, row: 2, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 3, text: "0-3", page: 0, row: 3, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 4, text: "0-4", page: 0, row: 4, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 5, text: "0-5", page: 0, row: 5, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 6, text: "0-6", page: 0, row: 6, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"visible"},
-        { id: 7, text: "0-7", page: 0, row: 7, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"visible"},
-        { id: 8, text: "0-8", page: 0, row: 8, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 9, text: "1-0", page: 0, row: 0, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 10,text: "1-1", page: 0, row: 1, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 11,text: "1-2", page: 0, row: 2, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 12,text: "1-3", page: 0, row: 3, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 13,text: "1-4", page: 0, row: 4, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 14,text: "1-5", page: 0, row: 5, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 15,text: "1-6", page: 0, row: 6, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 16,text: "1-7", page: 0, row: 7, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"},
-        { id: 17,text: "1-8", page: 0, row: 8, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:3}], visibility:"hidden"}
+        { id:"0",text: "0-0", page: 0, row: 0, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 1, text: "0-1", page: 0, row: 1, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 2, text: "0-2", page: 0, row: 2, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 3, text: "0-3", page: 0, row: 3, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 4, text: "0-4", page: 0, row: 4, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 5, text: "0-5", page: 0, row: 5, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 6, text: "0-6", page: 0, row: 6, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 7, text: "0-7", page: 0, row: 7, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 8, text: "0-8", page: 0, row: 8, column: 0, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 9, text: "1-0", page: 0, row: 0, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 10,text: "1-1", page: 0, row: 1, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 11,text: "1-2", page: 0, row: 2, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 12,text: "1-3", page: 0, row: 3, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 13,text: "1-4", page: 0, row: 4, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 14,text: "1-5", page: 0, row: 5, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 15,text: "1-6", page: 0, row: 6, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 16,text: "1-7", page: 0, row: 7, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"},
+        { id: 17,text: "1-8", page: 0, row: 8, column: 1, color: {r:0,g:0,b:0,a:255}, bgColor: {r:208,g:208,b:208,a:255}, actions: [{action:0}, {action:0}], visibility:"visible"}
         ];
 
 App.SSDEFAULTMENU = [{name: "Top", menuItems: App.SSMENUITEMS}];
@@ -225,10 +211,6 @@ App.SsMenuItemsController = Ember.ArrayController.extend({
 });
 
 
-App.SelectedSsMenuItemController = Ember.ObjectController.extend({
-	model: null
-});
-
 App.SsMenuItemView = Ember.View.extend({
   didInsertElement: function(){
     var self = this;
@@ -251,27 +233,17 @@ App.SsMenuItemView = Ember.View.extend({
 
 
 App.SsMenuItemController = Ember.ObjectController.extend({
-  needs: ["selected_ssMenuItem", "ssMenuItems", "ssActions"],
+  needs: ["selected_ssMenuItem", "ssMenuItems"],
   left: "0px",
   top: 1,
   align: "left",
   rowOffset: 45,
   border: "0px none black",
   opacity: 1,
-  columnOffset: "175px",
+  columnOffset: "173px",
   onCurrentPage: function(){
   	return this.get('controllers.ssMenuItems.currentPage') == this.get('page')
   }.property('page','controllers.ssMenuItems.currentPage'),
-	ssActions: function(){
-  	var model = this.get('model'),
-  	ref = model.get('ref');
-  	return EmberFire.ObjectArray.create({ref: ref.child('actions')});
-  	},
-  	ssActions: function(){
-  	var model = this.get('model'),
-  	ref = model.get('ref');
-  	return EmberFire.ObjectArray.create({ref: ref.child('actions')});
-  },
   menuStyle: function() {
   	var string;
   	if(this.get('column') == 1){
@@ -290,6 +262,7 @@ App.SsMenuItemController = Ember.ObjectController.extend({
 		string = 'border:'+ this.get('border')+
   		";opacity:"+ this.get('opacity')+
   		";text-align:"+ this.get('align')+
+  		";padding: 5px 5px 5px 5px"+
   		";left:" + this.get('left')+
   		";top:" + this.get('top')+
   		";color:rgba("+ this.get('color.r')+","+this.get('color.g')+","+this.get('color.b')+","+this.get('color.a')+")"+
@@ -308,37 +281,72 @@ App.SsMenuItemController = Ember.ObjectController.extend({
     select: function(){
       var model = this.get('model');
       this.set('controllers.selected_ssMenuItem.model', model);
-    this.set('controllers.ssActions.content', this.ssActions());
 
     }
   }
 });
 
+App.SelectedSsMenuItemController = Ember.ObjectController.extend({
+	model: null,
+	actionList: ["None", "Enter Submenu", "Submit Data + Enter Submenu", "Submit Data"],
+  	actionTypeList: ["Slide","Tap","Double Tap"],
+  	
+  	//slide action handling
+	slideActionText: function(key,value) { 
+	
+		if(arguments.length > 1){
+			var actionID;
+			for (var i=0; i< this.actionList.length; i++){
+				if(this.actionList[i]==value){
+					this.model.ref.child('actions/0/action').set(i);  //must go firebase here because setting an undefined variable fails				}
+				}
+			}
+		}
+		var actionID = this.get('model.actions.0.action')
+		return this.actionList[actionID];
+	}.property('model.actions.0.action'),
+	slideNeedsData: function() {
+		var actionID = this.get('model.actions.0.action')
+		if (actionID >1){ return true;}
+		return false;
+	}.property('model.actions.0.action'),
+	slideNeedsID: function() {
+		var actionID = this.get('model.actions.0.action')
+		if (actionID >0 && actionID < 3){ return true;}
+		return false;
+	}.property('model.actions.0.action'),
+	
+	// tap action handling
+	tapActionText: function(key,value) { 
+	
+		if(arguments.length > 1){
+			var actionID;
+			for (var i=0; i< this.actionList.length; i++){
+				if(this.actionList[i]==value){
+					this.model.ref.child('actions/1/action').set(i);  //must go firebase here because setting an undefined variable fails
+				}
+			}
+		}
+		var actionID = this.get('model.actions.1.action')
+		return this.actionList[actionID];
+	}.property('model.actions.1.action'),
+	tapNeedsData: function() {
+		var actionID = this.get('model.actions.1.action')
+		if (actionID <1){ return true;}
+		return false;
+	}.property('model.actions.1.action'),
+	tapNeedsID: function() {
+		var actionID = this.get('model.actions.1.action')
+		if (actionID >0 && actionID < 3){ return true;}
+		return false;
+	}.property('model.actions.1.action'),
+	actions: {
+		selectSlideActionItem: function (actionText) {
+			this.set('slideActionText',actionText);
+		},
+		selectTapActionItem: function (actionText) {
+			this.set('tapActionText',actionText);
+		}
+	}
 
-
-App.SsActionsController = Ember.ArrayController.extend({
-  ssActions: function(){
-    return Ember.ArrayProxy.create(
-      {content: this.get('content')});
-  }.property('content'),
-  actions: {
-  }
-});
-
-
-App.SelectedSsActionController = Ember.ObjectController.extend({
-	model: null
-});
-
-App.SsActionController = Ember.ObjectController.extend({
-  needs: ["selected_ssAction"],
-  selected: function(){
-    return this.get('controllers.selected_ssAction.model') === this.get('model');
-  }.property('controllers.selected_ssAction.model', 'model'),
-  actions: {
-    select: function(){
-      var model = this.get('model');
-      this.set('controllers.selected_ssAction.model', model);
-    }
-  }
 });
